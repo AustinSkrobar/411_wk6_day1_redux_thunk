6f61e89957f3dbfb87c212e7dfcb55534749f61c
Austin Skrobarczyk <austin.skrobar@gmail.com>
2021-03-01 22:32:17 -0600

    removing some comments and pushing up current stage

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,75 @@
-import React from 'react'
+import React from "react";
+import {
+  IconButton,
+  Menu,
+  MenuItem,
+  Container,
+  Button,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@material-ui/core";
+import { MoreVert } from "@material-ui/icons";
 
 const Import = (props) => {
-    // fill out this component
+  // fill out this component
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-    return (
-        <p>Import Component</p>
-    )
-}
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-export default Import
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Button variant="contained" color="primary" onClick={props.fetchMake}>
+        Import
+      </Button>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell align="right">Make</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.makes.map((make, idx) => (
+            <TableRow key={make.id}>
+              <TableCell component="th" scope="row">
+                {make.MakeId}
+              </TableCell>
+              <TableCell align="right">{make["MakeName"]}</TableCell>
+              <TableCell align="right">
+                <IconButton
+                aria-label="more"
+                aria-controls="long-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+                >
+                  <MoreVert />
+                </IconButton>
+                <Menu
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <p>Delete</p>
+                </Menu>
+            </TableCell>
+          </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Container>
+  );
+};
+
+export default Import;
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,15 +1,37 @@
-const url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
+const url =
+  "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json";
 
 export const addCar = (car) => {
-    return {
-        type: 'ADD_CAR',
-        value: car
-    }
-}
+  return {
+    type: "ADD_CAR",
+    value: car,
+  };
+};
 
 export const removeCar = (index) => {
+  return {
+    type: "REMOVE_CAR",
+    value: index,
+  };
+};
+
+export const fetchMake = () => {
+  return (dispatch) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then(response => {
+        const action = {
+          type: "FETCH_MAKES",
+          value: response.Results,
+        }
+        dispatch(action);
+      });
+  };
+};
+
+export const deleteMake = (index) => {
     return {
-        type: 'REMOVE_CAR',
-        value: index
-    }
-}
+        type: "DELETE_MAKES",
+        value: index,
+    };
+};
